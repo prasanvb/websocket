d535bb39053a7cbf4615488e0e40fe9edea150ed
prasanv <prasan.venkatb@gmail.com>
2024-11-11 19:34:34 -0800

    initial commit

---
+++ socket.io/server/src/index.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = 3030;
+
+// Global middlewares
+app.use(express.json());
+
+app.get('/', (_req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
